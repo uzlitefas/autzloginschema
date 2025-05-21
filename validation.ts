baac2494e9408c421712cfdb4cb30bbe53a9a272
uzlitefas <uzlitefas00@gmail.com>
2025-05-21 16:55:13 +0500

    Add files via upload

---
+++ validation.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+export const loginSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(8),
+});
+
+export const registerSchema = z
+	.object({
+		email: z.string().email(),
+		password: z.string().min(8),
+		confirmPassword: z.string(),
+	})
+	.refine(data => data.password === data.confirmPassword, {
+		message: 'Password do not match',
+		path: ['confirmPassword'],
+	});
